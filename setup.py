4e979014d582cea9cd45cee7d4839cb1f88df9d5
younho9 <younho9.choo@gmail.com>
2020-09-19 23:50:03 +0900

    chore: Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
     name="notion2github",
-    version="0.1",
+    version="0.1.1",
     description="A tool to use Notion as a Github Flavored Markdown(aka GFM) editor.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author="younho9",
     author_email="younho9.choo@gmail.com",
     url="https://github.com/younho9/notion2github",
-    download_url="",
+    download_url="https://github.com/younho9/notion2github/archive/v0.1.1.tar.gz",
     install_requires=[],
     packages=find_packages(exclude=[]),
     keywords=["notion", "github"],
