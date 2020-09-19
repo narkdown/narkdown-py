ccab75b122b626ec45540c104ebd470f3e2f326a
younho9 <younho9.choo@gmail.com>
2020-09-19 23:15:28 +0900

    feat: Update example.py

--- example.py
+++ example.py
@@ -1,23 +1,23 @@
 import sys
-from config import token, page_url, database_url
+from config import token, readme_url, docs_page_url, database_url
 from notion2github.exporter import NotionExporter
 
 if __name__ == "__main__":
     # Get project README.md
     NotionExporter(token, ".").get_notion_page(
-        url=page_url, create_page_directory=False
+        url=readme_url, create_page_directory=False
     )
 
     # Get directory README.md
     NotionExporter(token, "./docs").get_notion_page(
-        url=page_url, create_page_directory=False
+        url=docs_page_url, create_page_directory=False
     )
 
     # Get all contents from database
     NotionExporter(token).get_notion_pages_from_database(
         url=database_url,
         category_column_name="Category",
-        status_column_name="",
+        status_column_name="Status",
         current_status="",
         next_status="",
         filters={},
