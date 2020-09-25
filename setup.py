1b75f8ccd802a5c3280a636b1bcfa5ef920ecd39
Younho Choo <younho9.choo@gmail.com>
2020-09-25 18:47:22 +0900

    fix: Fix install dependencies

--- setup.py
+++ setup.py
@@ -1,28 +1,37 @@
-from setuptools import setup, find_packages
+import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
-setup(
+def get_requirements(fname):
+    "Takes requirements from requirements.txt and returns a list."
+    with open(fname) as fp:
+        reqs = list()
+        for lib in fp.read().split("\n"):
+            # Ignore pypi flags and comments
+            if not lib.startswith("-") or lib.startswith("#"):
+                reqs.append(lib.strip())
+        return reqs
+
+install_requires = get_requirements("requirements.txt")
+
+setuptools.setup(
     name="notion2github",
-    version="1.0.4",
+    version="1.0.5",
+    author="younho9",
+    author_email="younho9.choo@gmail.com",
     description="A tool to use Notion as a Github Flavored Markdown(aka GFM) editor.",
     long_description=readme,
     long_description_content_type="text/markdown",
-    author="younho9",
-    author_email="younho9.choo@gmail.com",
     url="https://github.com/younho9/notion2github",
-    download_url="https://github.com/younho9/notion2github/archive/v1.0.4.tar.gz",
-    install_requires=[],
-    packages=find_packages(exclude=[]),
+    install_requires=install_requires,
+    include_package_data=True,
+    packages=setuptools.find_packages(),
     keywords=["notion", "github"],
     python_requires=">=3.5",
-    package_data={},
-    zip_safe=False,
     classifiers=[
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
