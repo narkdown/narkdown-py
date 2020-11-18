e4633a9431123c4ca4e3f48e3699d5da2056798c
Younho Choo <younho9.choo@gmail.com>
2020-11-18 23:24:23 +0900

    release: v1.1.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires = get_requirements("requirements.txt")
 
 setuptools.setup(
     name="narkdown",
-    version="1.1.2",
+    version="1.1.3",
     author="younho9",
     author_email="younho9.choo@gmail.com",
     description="A tool to use Notion as a Markdown editor.",
